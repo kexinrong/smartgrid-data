35f1f48d512ec1d1d9991b1270674d04d72cc11d
Samuel Szuflita <piccolo@Hyperbolic-Time-Chamber.local>
2014-10-04 11:40:41 -0700

    Added methods to update labels if threshold conditions are not satisfied.

--- cluster.py
+++ cluster.py
@@ -21,18 +21,24 @@ ids = np.loadtxt(dataFile,
 	skiprows = 1,
 	usecols = [0])
 
+
 km = KMeans(n_clusters=minK, init='k-means++', max_iter=100, n_init=1)
 clusters = km.fit(data)
+
+
+def initialCentroids(clusterCenters):
+	centroidsMap = {}
+	for i in range(len(clusterCenters)):
+		centroidsMap[str(i)] = clusterCenters[i]
+	return centroidsMap
 
 def dataWithLabel(l, labels, data):
 	indices = []
 	for i in range(0, len(labels)):
 		if labels[i] == l:
 			indices.append(i)
-	S_l = []
-	for index in indices:
-		S_l.append(data[index])
-	return S_l
+	S_l = data[indices]
+	return (indices, S_l)
 
 def thresholdTest(S, centroid, theta):
 	totalDistance = 0
@@ -42,10 +48,24 @@ def thresholdTest(S, centroid, theta):
 	print totalDistance
 	return totalDistance < threshold
 
+def maxLabel(centroidMap):
+	maxLabel = None
+	for label in centroidsMap:
+		if (currentMax == None or int(label) > currentMax):
+			currentMax = int(label)
+	return maxLabel
 
+def updateLabels(S, indices, l, centroidMap, labels):
+	km = KMeans(n_clusters=2, init='k-means++', max_iter=100, n_init=1)
+	km.fit(S)
+	labelOffset = maxLabel(centroidMap) + 1
+	print km.labels_
 
-S_0 = dataWithLabel(0, km.labels_, data)
-print S_0
+centroidMap = initialCentroids(km.cluster_centers_)
+labels = km.labels_
+
+(indices, S_0) = dataWithLabel(0, km.labels_, data)
+updateLabels(S_0, indices, 0, centroidMap, labels)
 print thresholdTest(S_0, km.cluster_centers_, theta)
 
 
